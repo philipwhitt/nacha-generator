ea78d3e37c77d7904d7149a4000ba4a24f802d53
Philip W <philip.whitt@sbcglobal.net>
2015-09-14 15:04:34 -0500

    Adding amount field

--- test/Nacha/Field/AmountTest.php
+++ test/Nacha/Field/AmountTest.php
@@ -11,9 +11,17 @@ class AmountTest extends \PHPUnit_Framework_TestCase {
 		new Amount(10000000000.00); // only accepts $999M
 	}
 
-	public function testValid() {
+	public function testFloatOperations() {
 		// given
 		$sec = new Amount(100.99);
+
+		// then
+		$this->assertEquals('10099', (string)$sec);
+	}
+
+	public function testFloatStringOperations() {
+		// given
+		$sec = new Amount('100.99');
 
 		// then
 		$this->assertEquals('10099', (string)$sec);
