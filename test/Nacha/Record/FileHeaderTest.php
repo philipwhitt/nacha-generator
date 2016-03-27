8befde231193ade065d284d472e0871ff2c47256
Jeremy Forsythe <jdforsythe@gmail.com>
2016-03-26 22:44:11 -0400

    Failing test with long immediate origin (greater than PHP_INT_MAX)

--- test/Nacha/Record/FileHeaderTest.php
+++ test/Nacha/Record/FileHeaderTest.php
@@ -34,4 +34,21 @@ class FileHeaderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('101 051000033 059999997060210    A094101                                                      ', (string)$fileHeader);
 	}
 
+	public function testFileHeader_LongImmediateOrigin() {
+		// given
+		$fileHeader = (new FileHeader)
+			->setPriorityCode(1)
+			->setImmediateDestination('051000033')
+			->setImmediateOrigin('9059999997')
+			->setFileCreationDate('060210')
+			->setFileCreationTime('2232')
+			->setFormatCode('1')
+			->setImmediateDestinationName('ImdDest Name')
+			->setImmediateOriginName('ImdOriginName')
+			->setReferenceCode('Reference'); // will be truncated
+
+		$this->assertEquals(94, strlen($fileHeader));
+		$this->assertEquals('101 05100003390599999970602102232A094101ImdDest Name           ImdOriginName          Referenc', (string)$fileHeader);
+	}
+
 }
